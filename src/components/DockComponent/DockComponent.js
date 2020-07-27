ce79887475ab80fcd57086c3c27d4e916f16938e
Aakash Mangla <akkibaby90@yahoo.com>
2020-07-27 13:34:24 +0530

    Added contact page

--- src/components/DockComponent/DockComponent.js
+++ src/components/DockComponent/DockComponent.js
@@ -27,10 +27,10 @@ class Dock extends Component {
                             <span>Skills</span>
                             <Link to="/skills"><img src="https://image.flaticon.com/icons/svg/3079/3079012.svg"/></Link>
                         </li>
-                        <li>
+                        {/* <li>
                             <span>Experience</span>
                             <Link to="/experience"><img src="https://image.flaticon.com/icons/svg/2941/2941556.svg"/></Link>
-                        </li>
+                        </li> */}
                         <li>
                             <span>Hobbies</span>
                             <Link to="/hobbies"><img src="https://image.flaticon.com/icons/svg/2917/2917633.svg"/></Link>
