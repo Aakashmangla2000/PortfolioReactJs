f54b1dc4c673713d75cc2284998060c4f8ee2dfa
Aakash Mangla <akkibaby90@yahoo.com>
2020-07-27 12:23:49 +0530

    Projects page done

--- src/components/AboutComp/About.js
+++ src/components/AboutComp/About.js
@@ -6,12 +6,11 @@ function About() {
         <div className="aboutUs">
             <div className="row">
                 <div className="col-md-6 leftside">
-                <h1 className="headin text-success">I'm Aakash Mangla</h1>
-                <h6>Practicing Frontend Developer with a demonstrated history
-                of working in the e-learning industry. Skilled in Firebase,
-                Python (Programming Language), C++, Management, and Teamwork.
-                Strong engineering professional with a BTech focused on Information
-                Technology from Bhartiya VidyaPeeth College Of Engineering, New Delhi . </h6>
+                <h1 className="headin text-primary">I'm Aakash Mangla</h1>
+                <h6>Full Stack develeoper based in Delhi. Skilled in MERN Stack,
+                Python, C++, Management, and Teamwork.
+                Strong engineering professional with a B.Tech focused on Information
+                Technology from Bhartiya Vidyapeeth College Of Engineering, New Delhi. </h6>
                 </div>
                 <div className="col-md-6">
                 <img src="/images/me.jpeg" className="my-img"></img>
--- src/components/ProjectsComp/Projects.js
+++ src/components/ProjectsComp/Projects.js
@@ -1,21 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './projects.css'
 
-function Projects() {
-    return (
-        <div>
-            Projects
-            <div className="row">
-                <div className="col-md-8">
+export class Projects extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            projects: [{"name":"Classic iPod","link":"https://ipod-reactjs.web.app/","vid":"ezgif.com-video-to-gif.gif"},{"name":"Covid-19 Tracker","link":"https://aakashmangla2000.github.io/Covid-19-India-Website/","vid":"ezgif.com-video-to-gif copy.gif"},{"name":"ATTAC","link":"https://docs.google.com/presentation/d/1E0xVpB25lAGZwybCTavIRpZfUZv8Z1R_nCv3VcXBrlo/edit#slide=id.g35f391192_00","vid":"ezgif.com-video-to-gif-3.gif"},{"name":"Tre-Way","link":"https://tre-way.web.app/","vid":"ezgif.com-video-to-gif-2.gif"}],
+            active: 0,
+            clicked: 0
+        }
+        this.elementClicked = this.elementClicked.bind(this)
+    }
+
+    elementClicked(ind){
+        this.setState({
+            clicked: ind
+        })
+    }
+    
+    render() {
+
+        let proj = this.state.projects.map((el,ind) => {
+            return(
+                <li className={`${this.state.clicked===ind?'active':''} pro-el text-primary`}><div onMouseEnter={() => this.elementClicked(ind)}>{this.state.projects[ind].name}    <a target="_blank" href={this.state.projects[ind].link}><img className={`${this.state.clicked===ind?'active2':''} im-link`} src="https://image.flaticon.com/icons/svg/922/922124.svg"></img></a></div></li>
+            )
+        })
+
+        return (
+            <div>
+                <div className="row">
+                <div className="col-md-9">
+                    {/* <div className="macbook"></div> */}
+                    <img className="screen" src={`images/${(this.state.projects[this.state.clicked]).vid}`}></img>
                     <img className="macbook" src="/images/pngflow.com (1).png"></img>
                 </div>
-                <div className="col-md-4">
-
+                <div className="col-md-3 lss">
+                    <ul className="ls">
+                        {/* <li><a target="_blank" href="https://ipod-reactjs.web.app/">Classic iPod</a></li>
+                        <li><a target="_blank" href="https://aakashmangla2000.github.io/Covid-19-India-Website/">Covid-19 Tracker</a></li>
+                        <li><a target="_blank" href="https://docs.google.com/presentation/d/1E0xVpB25lAGZwybCTavIRpZfUZv8Z1R_nCv3VcXBrlo/edit#slide=id.g35f391192_00">ATTAC</a></li>
+                        <li><a target="_blank" href="https://tre-way.web.app/">Tre-Way</a></li> */}
+                        {proj}
+                    </ul>
                 </div>
-
             </div>
-        </div>
-    )
+            </div>
+        )
+    }
 }
 
 export default Projects
+
