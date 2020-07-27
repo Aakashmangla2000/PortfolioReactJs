6a1308d928fddf486251d0ca4a57e6ab2e065a3e
Aakash Mangla <akkibaby90@yahoo.com>
2020-07-27 15:52:49 +0530

    MVP website complete

--- src/components/HobbiesComp/Hobbies.js
+++ src/components/HobbiesComp/Hobbies.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './hobbies.css'
 
-function Hobbies() {
-    return (
-        <div>
-            Hobbies
-        </div>
-    )
+class Hobbies extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             index:0,
+             imgs: ['BEDE4C6F-B72F-4FAE-A0F4-8241067A55E8_1_105_c.jpeg','IMG_8078.jpeg','IMG_7860.jpeg'],
+             hob: ['Rubiks Cube','Cooking','Photography']
+        }
+        this.clicked = this.clicked.bind(this)
+    }
+
+    clicked(i){
+        if(this.state.index+i<=2&&this.state.index+i>=0)
+        this.setState({
+            index: this.state.index+i
+        })
+        else if(this.state.index+i<0)
+        this.setState({
+            index: 2
+        })
+        else
+        this.setState({
+            index: 0
+        })
+        console.log(this.state.index)
+    }
+    
+    render() {
+        return (
+            <div>
+                <div className="car-body">
+                <img src={`images/${this.state.imgs[this.state.index]}`} className="car-img"></img>
+                <h5 className="headingg">{this.state.hob[this.state.index]}</h5>
+                <div  onClick={() => this.clicked(1)} ><img src="https://image.flaticon.com/icons/svg/929/929736.svg"className="butn1"></img></div>
+                <div onClick={() => this.clicked(-1)} ><img src="https://image.flaticon.com/icons/svg/929/929731.svg" className="butn2"></img></div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Hobbies
--- src/components/SkillsComp/Skills.js
+++ src/components/SkillsComp/Skills.js
@@ -1,11 +1,141 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './skills.css'
 
-function Skills() {
-    return (
-        <div>
-            Skills
-        </div>
-    )
+export class Skills extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             skills:[
+                {
+                    "name":"Programming Languages",
+                    "sub": [
+                        {
+                            "name":"C++",
+                            "val": 80
+                        },
+                        {
+                            "name":"Python",
+                            "val": 70
+                        },
+                        {
+                            "name":"JavaScript",
+                            "val": 90
+                        },
+                        {
+                            "name":"Java",
+                            "val": 50
+                        }
+                    ],
+                    "link": "https://image.flaticon.com/icons/svg/3176/3176336.svg"
+                },
+                {
+                    "name":"Front-End",
+                    "sub": [
+                        {
+                            "name":"HTML",
+                            "val": 90
+                        },
+                        {
+                            "name":"CSS",
+                            "val": 90
+                        },
+                        {
+                            "name":"React.Js",
+                            "val": 90
+                        },
+                        {
+                            "name":"Redux",
+                            "val": 60
+                        }
+                    ],
+                    "link": "https://image.flaticon.com/icons/svg/646/646460.svg"
+                },
+                {
+                    "name":"Back-End",
+                    "sub": [
+                        {
+                            "name":"Node.Js",
+                            "val": 80
+                        },
+                        {
+                            "name":"Express",
+                            "val": 30
+                        },
+                        {
+                            "name":"REST API",
+                            "val": 90
+                        },
+                        {
+                            "name":"SQL/No-SQL",
+                            "val": 70
+                        }
+                    ],
+                    "link": "https://image.flaticon.com/icons/svg/2165/2165703.svg"
+                }
+             ],
+             index: 0
+        }
+        this.hoverElement = this.hoverElement.bind(this)
+    }
+
+
+    hoverElement(ind){
+        this.setState({
+            index: ind
+        })
+    }
+
+    render() {
+        let cols = ['red','blue','green','orange']
+
+        let list = this.state.skills[this.state.index].sub.map((el,ind) => {
+            return(
+                <div class="single-chart">
+                    {/* <h6>{el.name}</h6> */}
+                    <svg viewBox="0 0 36 36" class={`circular-chart ${cols[ind]}`}>
+                    <path class="circle-bg"
+                        d="M18 2.0845
+                        a 15.9155 15.9155 0 0 1 0 31.831
+                        a 15.9155 15.9155 0 0 1 0 -31.831"
+                    />
+                    <path class="circle"
+                        stroke-dasharray={`${el.val}, 100`}
+                        d="M18 2.0845
+                        a 15.9155 15.9155 0 0 1 0 31.831
+                        a 15.9155 15.9155 0 0 1 0 -31.831"
+                    />
+                    <text x="18" y="20.35" class="percentage">{el.name}</text>
+                    </svg>
+                </div>
+            )
+        })
+
+        let list2 = this.state.skills.map((el,ind) => {
+            return(
+                <div className="left-list" onMouseEnter={() => {this.hoverElement(ind)}}><li><h4>{el.name} <img className="logo" src={el.link}></img></h4></li></div>
+            )
+        })
+        return (
+            <div>
+                <div className="flex-wrapper justify-content-center">
+                <div className="row">
+                    <div className="col-md-9 justify-content-center left-side">
+                        <ul>
+                            {list2}
+                        </ul>
+                    </div>
+                    <div className="col-md-3 right-side">
+                        {/* <div className="row"> */}
+                            {list}
+                        {/* </div> */}
+                    </div>
+                </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Skills
