cf59e79ebe9b889444dc6b26521ef6268b2a1cd1
Aakash Mangla <akkibaby90@yahoo.com>
2020-07-31 23:10:42 +0530

    Fixed a bug

--- src/components/AboutComp/About.js
+++ src/components/AboutComp/About.js
@@ -7,8 +7,8 @@ function About() {
             <div className="row">
                 <div className="col-md-6 leftside">
                 <h1 className="headin">Hi, I'm Aakash Mangla</h1>
-                <h6>Full Stack develeoper based in Delhi. Skilled in MERN Stack,
-                Python, C++, Management, and Teamwork.
+                <h6>Full Stack developer based in Delhi. Skilled in MERN Stack,
+                Python, C++, Project Management, and Teamwork.
                 Strong engineering professional with a B.Tech focused on Information
                 Technology from Bhartiya Vidyapeeth College Of Engineering, New Delhi. </h6>
                 </div>
