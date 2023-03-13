d60630ec26812aef0bf712788e8fe126f5b664a2
aakashmangla2000 <akkibaby90@yahoo.com>
2023-03-13 18:52:27 +0530

    Fix experience page order and style

--- src/components/ExperienceComp/Experience.js
+++ src/components/ExperienceComp/Experience.js
@@ -12,20 +12,85 @@ function Experience() {
 
       <ul class="timeline" style={{ textAlign: "justify" }}>
         <li>
+          <div class="direction-l">
+            <div class="flag-wrapper">
+              <span class="flag">ThoughtWorks Technologies</span>
+              <span class="time-wrapper">
+                <span class="time">Aug 2022 - March 2023</span>
+              </span>
+            </div>
+            <div class="desc">
+              <h5 style={{ fontSize: "20px" }}>Developer</h5> <br />
+              <div style={{ textAlign: "justify" }}>
+                • Worked as a Full Stack Developer on a TechOps Project.
+                <br></br>• Assisted in effortless transition and development of
+                new UI design for the product from scratch.
+                <br></br>• Collaborated with the team in development of new
+                features as per the business requirements.
+                <br></br>• TechStack:
+                ReactJS,NextJS,SpringBoot,CircleCI,GCP,PostgreSQL.
+                <br></br>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="direction-r">
+            <div class="flag-wrapper">
+              <span class="flag">ThoughtWorks Technologies</span>
+              <span class="time-wrapper">
+                <span class="time">Mar 2022 - Aug 2022</span>
+              </span>
+            </div>
+            <div class="desc">
+              <h5 style={{ fontSize: "20px" }}>Developer - Intern</h5> <br />
+              <div style={{ textAlign: "justify" }}>
+                Working as a Developer Intern at ThoughtWorks.
+                <br></br>
+                Worked with System Designing, Design Patterns, Agile Practices
+                and Front-End Development.
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="direction-l">
+            <div class="flag-wrapper">
+              <span class="flag">IHS Markit (S&P Global)</span>
+              <span class="time-wrapper">
+                <span class="time">Jun 2021 - Jul 2021</span>
+              </span>
+            </div>
+            <div class="desc">
+              <h5 style={{ fontSize: "20px" }}>Software Engineering Intern</h5>{" "}
+              <br />
+              <div style={{ textAlign: "justify" }}>
+                • Implemented Web APIs using ASP.NET Core, Repository Pattern
+                and MVC Architecture successfully. <br />
+                • Worked with Team Apollo on the products Market EyeQ and
+                Enterprise EyeQ. <br />• Collaborated on more than 10 features
+                with the team and wrote unit and integration tests for the same.
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
           <div class="direction-r">
             <div class="flag-wrapper">
               <span class="flag">Bachelor of Technology (B.Tech)</span>
               <span class="time-wrapper">
-                <span class="time">2018 - present</span>
+                <span class="time">2018 - 2022</span>
               </span>
             </div>
             <div class="desc">
               <h5 style={{ fontSize: "20px" }}>Information Technology</h5>{" "}
               <br />
-              CGPA: 8.56 <br />
-              Coursework: Data Structure, Object Oriented Programming,
-              Algorithms Design and Analysis, Software Engineering, Operating
-              Systems, Databases, Web Engineering.
+              <div style={{ textAlign: "justify" }}>
+                CGPA: 8.56 <br />
+                Coursework: Data Structure, Object Oriented Programming,
+                Algorithms Design and Analysis, Software Engineering, Operating
+                Systems, Databases, Web Engineering.
+              </div>
             </div>
           </div>
         </li>
@@ -50,67 +115,6 @@ function Experience() {
             </div>
           </div>
         </li> */}
-
-        <li>
-          <div class="direction-l">
-            <div class="flag-wrapper">
-              <span class="flag">IHS Markit (S&P Global)</span>
-              <span class="time-wrapper">
-                <span class="time">Jun 2021 - Jul 2021</span>
-              </span>
-            </div>
-            <div class="desc">
-              <h5 style={{ fontSize: "20px" }}>Software Engineering Intern</h5>{" "}
-              <br />
-              •Implemented Web APIs using ASP.NET Core, Repository Pattern and
-              MVC Architecture successfully. <br />
-              •Worked with Team Apollo on the products Market EyeQ and
-              Enterprise EyeQ. <br />
-              •Collaborated on more than 10 features with the team and wrote
-              unit and integration tests for the same.
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="direction-r">
-            <div class="flag-wrapper">
-              <span class="flag">ThoughtWorks Technologies</span>
-              <span class="time-wrapper">
-                <span class="time">Mar 2022 - Aug 2022</span>
-              </span>
-            </div>
-            <div class="desc">
-              <h5 style={{ fontSize: "20px" }}>Developer Intern</h5> <br />
-              Working as a Developer Intern at ThoughtWorks.
-              <br></br>
-              Worked with System Designing, Design Patterns, Agile Practices and
-              Front-End Development.
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="direction-l">
-            <div class="flag-wrapper">
-              <span class="flag">ThoughtWorks Technologies</span>
-              <span class="time-wrapper">
-                <span class="time">Aug 2022 - March 2023</span>
-              </span>
-            </div>
-            <div class="desc">
-              <h5 style={{ fontSize: "20px" }}>Developer</h5> <br />
-              •Worked as a Full Stack Developer on a TechOps Project.
-              <br></br>
-              •Assisted in effortless transition and development of new UI
-              design for the product from scratch.
-              <br></br>
-              •Collaborated with the team in development of new features as per
-              the business requirements.
-              <br></br>
-              •TechnologiesUsed-ReactJS,NextJS,SpringBoot,CircleCI,GCP,PostgreSQL.
-              <br></br>
-            </div>
-          </div>
-        </li>
       </ul>
       <br />
       <br />
